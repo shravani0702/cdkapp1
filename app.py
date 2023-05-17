d358057ad965e99504f29b38db154f8c0148ea5f
Shravani <chinnamshravani@gmail.com>
2023-05-17 11:59:38 -0400

    all files

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from hell_cdk.hell_cdk_stack import HellCdkStack
+from hell_cdk.hell_cdk_stack import hellcdkstack
 
 
 app = cdk.App()
-HellCdkStack(app, "HellCdkStack",
+hellcdkstack(app, "hellcdkstack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -15,14 +15,14 @@ HellCdkStack(app, "HellCdkStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    #env=cdk.Environment(account='242364459859', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
-
+    
 app.synth()
--- tests/unit/test_hell_cdk_stack.py
+++ tests/unit/test_hell_cdk_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from hell_cdk.hell_cdk_stack import HellCdkStack
+from hell_cdk.hell_cdk_stack import hellcdkstack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in hell_cdk/hell_cdk_stack.py
+# resource in cdk_project/cdk_project_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = HellCdkStack(app, "hell-cdk")
+    stack = hellcdkstack(app, "hell_cdk")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
